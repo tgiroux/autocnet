55b543db734d2ac5cf08ebc34283d2d012c6040f
Jay <jlaura@asu.edu>
2019-06-03 21:34:53 -0700

    initial stab at postgresql in tests

---
+++ autocnet/io/db/tests/test_model.py
@@ -0,0 +1,57 @@
+import pytest
+from autocnet.io.db import model
+from autocnet import Session, engine
+
+@pytest.fixture
+def tables():
+    return engine.table_names()
+
+@pytest.fixture
+def session(tables, request):
+    session = Session()
+
+    def cleanup():
+        for t in reversed(tables):
+            session.execute(t.delete())
+        session.commit()
+
+    request.addfinalizer(cleanup)
+    
+    return session
+
+
+def test_keypoints_exists(tables):
+    assert model.Keypoints.__tablename__ in tables
+
+def test_edges_exists(tables):
+    assert model.Edges.__tablename__ in tables
+
+def test_costs_exists(tables):
+    assert model.Costs.__tablename__ in tables
+
+def test_matches_exists(tables):
+    assert model.Matches.__tablename__ in tables
+
+def test_cameras_exists(tables):
+    assert model.Cameras.__tablename__ in tables
+
+def test_images_exists(tables):
+    assert model.Images.__tablename__ in tables
+
+def test_create_image():
+    i = model.Images()
+    session.add(i)
+    session.commit()
+    res_i = session.query(Images).first()
+    print(i.id)
+    print(res_i.id)
+    session.close()
+
+def test_overlay_exists(tables):
+    assert model.Overlay.__tablename__ in tables
+
+def test_points_exists(tables):
+    assert model.Points.__tablename__ in tables
+
+def test_measures_exists(tables):
+    assert model.Measures.__tablename__ in tables
