577d4aff5eb8ca77ce2d1e9b7ae9eeca2c0eba80
Kelvin Rodriguez <kr788@nau.edu>
2020-01-30 15:32:29 -0700

    Updates to hirise change detection  (#411)
    * added hirise coreg + updated to_isis to match network version
    
    * updated matcher
    
    * faster match, hirise update
    
    * added okubo method
    
    * things workin
    
    * updated hirise notebook
    
    * updated okubogar
    
    * patched some things up
    
    * added example images

--- autocnet/cg/change_detection.py
+++ autocnet/cg/change_detection.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from matplotlib.path import Path
 from shapely.geometry import Point, MultiPoint
@@ -11,56 +10,63 @@ from autocnet.utils.utils import bytescale
 from autocnet.matcher.cpu_extractor import extract_features
 
 def image_diff(arr1, arr2):
-    diff = arr1-arr2
-    diff[np.isnan(diff)] = 0
+     arr1 = arr1.astype("float32")
+     arr2 = arr2.astype("float32")
+     arr1[arr1 == 0] = np.nan
+     arr2[arr2 == 0] = np.nan
 
-    bdiff = bytescale(diff)
-    return bdiff
+     diff = arr1-arr2
+     diff[np.isnan(diff)] = 0
+
+     return bytescale(diff)
 
 
-def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", extractor_kwargs={"nfeatures": 2000, "scaleFactor": 1.1, "nlevels": 1}, image_func=image_diff):
-    arr1 = image1.read_array()
-    arr2 = image2.read_array()
-    arr1[arr1 == arr1.min()] = np.nan
-    arr2[arr2 == arr2.min()] = np.nan
+def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", extractor_kwargs={"nfeatures": 2000, "scaleFactor": 1.1, "nlevels": 1}, cluster_params={"min_samples": 10, "max_eps": 10, "eps": .5, "xi":.5},  image_func=image_diff):
+     arr1 = image1.read_array()
+     arr2 = image2.read_array()
+     arr1[arr1 == arr1.min()] = 0
+     arr2[arr2 == arr2.min()] = 0
+     arr1 = bytescale(arr1)
+     arr2 = bytescale(arr2)
 
-    bdiff = image_func(arr1, arr2)
+     bdiff = image_func(arr1, arr2)
 
-    keys, descriptors = extract_features(bdiff, extractor_method, extractor_parameters=extractor_kwargs)
-    x,y = keys["x"], keys["y"]
+     keys, descriptors = extract_features(bdiff, extractor_method, extractor_parameters=extractor_kwargs)
+     x,y = keys["x"], keys["y"]
 
-    points = [Point(xval, yval) for xval,yval in zip(x,y)]
+     points = [Point(xval, yval) for xval,yval in zip(x,y)]
 
-    optics = OPTICS(min_samples=10, max_eps=20,  eps=.3, p=2, xi=.5).fit(list(zip(x,y)))
+     optics = OPTICS(**cluster_params).fit(list(zip(x,y)))
 
-    classes = gpd.GeoDataFrame(columns=["label", "point"], geometry="point")
-    classes["label"] = optics.labels_
-    classes["point"] = points
-    class_groups = classes.groupby("label").groups
+     classes = gpd.GeoDataFrame(columns=["label", "point"], geometry="point")
+     classes["label"] = optics.labels_
+     classes["point"] = points
+     class_groups = classes.groupby("label").groups
 
-    polys = []
-    weights = []
+     polys = []
+     weights = []
 
-    # array of x,y pairs
-    xv, yv = np.mgrid[0:bdiff.shape[1], 0:bdiff.shape[0]]
+     # array of x,y pairs
+     xv, yv = np.mgrid[0:bdiff.shape[1], 0:bdiff.shape[0]]
 
-    for label, indices in class_groups.items():
-        if label == -1:
-            continue
+     for label, indices in class_groups.items():
+         if label == -1:
+             continue
 
-        points = classes.loc[indices]["point"]
-        poly = MultiPoint(points.__array__()).convex_hull
-        xmin, ymin, xmax, ymax = np.asarray(poly.bounds).astype("uint64")
-        xv, yv = np.mgrid[xmin:xmax, ymin:ymax]
-        xv = xv.flatten()
-        yv = yv.flatten()
+         points = classes.loc[indices]["point"]
+         poly = MultiPoint(points.__array__()).convex_hull
+         xmin, ymin, xmax, ymax = np.asarray(poly.bounds).astype("uint64")
+         xv, yv = np.mgrid[xmin:xmax, ymin:ymax]
+         xv = xv.flatten()
+         yv = yv.flatten()
 
-        points = np.vstack((xv,yv)).T.astype("uint64")
+         points = np.vstack((xv,yv)).T.astype("uint64")
 
-        mask = Path(np.asarray(poly.exterior.xy).T.astype("uint64")).contains_points(points).reshape(int(ymax-ymin), int(xmax-xmin))
-        weight = bdiff[ymin:ymax,xmin:xmax].mean()
+         mask = Path(np.asarray(poly.exterior.xy).T.astype("uint64")).contains_points(points).reshape(int(ymax-ymin), int(xmax-xmin))
+         weight = bdiff[ymin:ymax,xmin:xmax].mean()
 
-        polys.append(poly)
-        weights.append(weight)
+         polys.append(poly)
+         weights.append(weight)
 
-    return polys, weights
+     return polys, weights, bdiff
+
