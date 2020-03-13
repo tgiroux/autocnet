5df2f88e09c969c1cc7634bb9fb9bfe2b4165fb4
jlaura <jlaura@usgs.gov>
2020-03-13 13:06:28 -0700

    Update deploy.sh
    No need for if blocks when we can use the travis.yml to constrain to particular branches

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    travis-sphinx deploy
-    source deactivate
-    conda install conda-build anaconda-client
-    conda config --set anaconda_upload yes
-    conda build --token $CONDA_UPLOAD_TOKEN --python $PYTHON_VERSION -c conda-forge -c menpo -c usgs-astrogeology conda
-fi
+travis-sphinx deploy
+conda deactivate
+conda install conda-build anaconda-client
+conda config --set anaconda_upload yes
+conda build --token $CONDA_UPLOAD_TOKEN --python $PYTHON_VERSION -c conda-forge -c menpo -c usgs-astrogeology conda
