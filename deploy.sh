751ee2874b57c45ce2c02341663ab86eff3f7d9d
jlaura <jlaura@usgs.gov>
2020-03-13 15:49:08 -0700

    Update deploy.sh
    Trying just a doc deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 travis-sphinx deploy
-conda deactivate
-conda install conda-build anaconda-client
-conda config --set anaconda_upload yes
-conda build --token $CONDA_UPLOAD_TOKEN --python $PYTHON_VERSION -c conda-forge -c menpo -c usgs-astrogeology conda
+# source deactivate
+# conda install conda-build anaconda-client
+# conda config --set anaconda_upload yes
+# conda build --token $CONDA_UPLOAD_TOKEN --python $PYTHON_VERSION -c conda-forge -c menpo -c usgs-astrogeology conda
