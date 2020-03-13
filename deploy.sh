f3abe2a13ea4708927c262e4b3acb0b35ce8931e
jlaura <jlaura@usgs.gov>
2020-03-13 16:10:27 -0700

    Update deploy.sh
    travis-sphinx defaults to deploying only on the master branch. Need to also pass the `-b` flag to build on dev.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-travis-sphinx -v deploy
-# source deactivate
-# conda install conda-build anaconda-client
-# conda config --set anaconda_upload yes
-# conda build --token $CONDA_UPLOAD_TOKEN --python $PYTHON_VERSION -c conda-forge -c menpo -c usgs-astrogeology conda
+travis-sphinx -v deploy -b dev
+source deactivate
+conda install conda-build anaconda-client
+conda config --set anaconda_upload yes
+conda build --token $CONDA_UPLOAD_TOKEN --python $PYTHON_VERSION -c conda-forge -c menpo -c usgs-astrogeology conda
